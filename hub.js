fe3b2027d024872d4048ddb469b1d8e468065236
adeutry <adeutry@temple.edu>
2015-06-30 17:24:44 -0400

    implemented reply columns

--- hub.js
+++ hub.js
@@ -11,18 +11,31 @@ $.ajax({
     }
 });
 
-$(".content_div").click(function(event) { 
-	var data = event.target.id;
-	console.log("Got data: " + data);
-	var col = data.charAt(data.indexOf("_div_")+5);
-	var board = data.slice( data.indexOf("_div_") + 7,data.lastIndexOf("_"));
-	var postNo = data.slice(data.lastIndexOf("_")+1);
-	console.log("col: " + col +"\nboard: " + board + "\npostNo :" + postNo );
-	getPostData(board, postNo);
- });
 
 
-function getPostData ( board , postNo)
+function createThreadBox( data,col , board , postNo)
+{
+
+	var offset = $("#content_div_"+col+"_"+board+"_"+postNo).offset().top;
+	var column = "<div style=\"margin-top: "+ offset +"px;\" id=\"thread_column_" + board + "_" + postNo + "\" class=\"thread_column\" >";
+		var posts = data['posts'];
+		var replyDiv;
+		for (var i = 0 ; i < posts.length ; i++ )
+		{
+			replyDiv = "<div id=\"reply_div_" + posts[i]['no'] + "\" class=\"reply_div\">";
+				if(posts[i]['tim'] != null){
+				replyDiv += "<img width =\"70\" height=\"70\" src=\"http://i.4cdn.org/" + board + "/" + posts[i]['tim'] +"s.jpg\" >"; }
+				replyDiv += posts[i].com;		
+			replyDiv += "</div>";
+			column+=replyDiv;
+		}
+	column += "</div>";
+	$("#content_column_" + col).after(column);
+
+
+} 
+
+function getPostData( col,board , postNo)
 {
 	var posts;
 	var url= "https://a.4cdn.org/" + board + "/thread/" + postNo +".json";
@@ -32,14 +45,30 @@ function getPostData ( board , postNo)
 	    dataType: 'json',
 	    success:  function (data) 
             {
-		posts = data['posts'];
-		for(var i =0; i < posts.length ; i++)
-		{
-			console.log(posts[i]['com']);
-		}
+		createThreadBox(data , col , board , postNo);		
             }
 	});
 	
-
+	return posts;
 
 }
+
+
+
+
+
+
+
+$(".content_div").click(function(event) { 
+	var data = event.target.id;
+	console.log("Got data: " + data);
+	var col = data.charAt(data.indexOf("_div_")+5);
+	var board = data.slice( data.indexOf("_div_") + 7,data.lastIndexOf("_"));
+	var postNo = data.slice(data.lastIndexOf("_")+1);
+	console.log("col: " + col +"\nboard: " + board + "\npostNo :" + postNo );
+	getPostData(col,board, postNo)
+
+ });
+
+
+
